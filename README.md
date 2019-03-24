f66094dbf38c47dfcd116cda48af6d9bdb8b6a5e
zshao1031 <terry1031@mail2000.com.tw>
2019-03-24 16:47:23 +0800

    建立專案

---
+++ README.md
@@ -0,0 +1,8 @@
+## 【練習作品】
+***
+### 主題：手機頁面滑動（Mobile Page Slide）
+### 技術：html + SCSS + jQuery
+### 頁面展示：<https://zshao1031.github.io/MobilePageSlide/>
+
+
+
---
+++ js/all.js
@@ -0,0 +1,14 @@
+//等網頁全部載入後才會執行的程式碼(JavaScript版)
+/*
+window.onload = function () { 
+	
+}
+*/
+
+//等網頁全部載入後才會執行的程式碼(jQuery版)
+/*
+$(document).ready(function() {
+
+});
+*/
+
